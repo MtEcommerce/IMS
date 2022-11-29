1fa113a25f77a7d15082c8e2c503c8a2132f3f21
Martin Lin <martin.lin.@mexc.com>
2022-11-29 11:36:10 +0800

    chore: Add `Tailwind CSS` and `MUI base`

---
+++ components/Layout/Header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="text-3xl font-bold underline">
+      <div>Header Red</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/Layout/SideMenu/index.tsx
@@ -0,0 +1,5 @@
+const SideMenu = () => {
+  return <aside>Side Menu</aside>;
+};
+
+export default SideMenu;
